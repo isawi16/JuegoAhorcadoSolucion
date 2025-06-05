d0e537707277870c12fec146a0a796667061df35
LilHoneyBunny <zs19014025@gmail.com>
2025-06-05 15:58:01 -0600

    adiciones ventanas
    Se agregan UserControls, se modifica AhorcadoService para añadir métodos, se agrega DTO de palabra

---
+++ ClienteAhorcado/Vistas/ConsultarHistorialPartidasUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClienteAhorcado.Vistas
+{
+    /// <summary>
+    /// Interaction logic for ConsultarHistorialPartidasUserControl.xaml
+    /// </summary>
+    public partial class ConsultarHistorialPartidasUserControl : UserControl
+    {
+        public ConsultarHistorialPartidasUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
