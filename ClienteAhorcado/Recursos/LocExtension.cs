26ed85953725db6dffe99b4f6d1225f3525ac421
LilHoneyBunny <zs19014025@gmail.com>
2025-06-18 19:52:03 -0600

    Se agregan clases de internacionalización

---
+++ ClienteAhorcado/Recursos/LocExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace ClienteAhorcado.Recursos
+{
+   public class LocExtension : MarkupExtension
+   {
+        public string Key { get; set; }
+
+        public LocExtension(string key)
+        {
+            Key = key;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Properties.Resources.ResourceManager.GetString(Key, CultureInfo.CurrentUICulture) ?? $"!{Key}!";
+        }
+    }
+    }
+
